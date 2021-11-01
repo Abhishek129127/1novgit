32ed9678b304ec0f3b08c867328f87e1679f3628
Abhishek129127 <Abhishek.Desai@lntinfotech.com>
2021-11-01 10:09:40 +0530

    Add project files.

---
+++ 1novgit/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _1novgit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
