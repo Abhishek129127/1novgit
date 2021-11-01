90aa1121e0492bddb15e555e006052e5e596313e
Abhishek129127 <Abhishek.Desai@lntinfotech.com>
2021-11-01 11:43:53 +0530

    message

--- 1novgit/Program.cs
+++ 1novgit/Program.cs
@@ -10,7 +10,7 @@ namespace _1novgit
             Console.WriteLine("hi");
             Console.WriteLine("hello");
             Console.WriteLine("abhi");
-            Console.WriteLine("amit");
+            Console.WriteLine("ameet");
         }
     }
 }
